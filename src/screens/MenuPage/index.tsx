a0662c279bf24cc23d776e6171f6ae0e5be5e492
weedeej <work.deeej@gmail.com>
2024-03-08 11:11:19 +0800

    used menu list and addmenu section

--- src/screens/MenuPage/index.tsx
+++ src/screens/MenuPage/index.tsx
@@ -1,3 +1,28 @@
+import { Menu } from "@/types";
+import { Box, Button, Stack, colors } from "@mui/material";
+import { useState } from "react"
+import { MenuList } from "./MenuList";
+import { Add } from "@mui/icons-material";
+import { AddMenuSection } from "./AddMenuSection";
+import { useSelector } from "react-redux";
+import { StoreState } from "@/redux/store";
+
 export function MenuPage() {
-  return <>Menu</>
+  const menu = useSelector((state: StoreState) => state.menus.list);
+  const categories = useSelector((state: StoreState) => state.categories.list);
+
+  return (
+    <Stack width="100%" gap={2}>
+      {
+        menu && <AddMenuSection categories={categories} menuList={menu}/>
+      }
+      <Stack
+        direction="row"
+        justifyContent={(menu && menu.length > 0) ? "space-evenly" : "center"}
+        alignItems="stretch"
+        flexWrap="wrap">
+        <MenuList menuList={menu} />
+      </Stack>
+    </Stack>
+  )
 }
